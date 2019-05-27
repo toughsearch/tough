d61367ebc6367564fb9b5ef09f3e0c77772eabb0
Dima Doroshev <dima@doroshev.com>
2019-05-27 20:07:53 +0300

    strptime

--- tough/utils.py
+++ tough/utils.py
@@ -1,9 +1,54 @@
+import ctypes
+from ctypes.util import find_library
 from datetime import datetime, timedelta, timezone
 import os
 import re
 
 from . import indexes
 from .config import INDEX_DIR
+
+
+libc = ctypes.CDLL(find_library('c'))
+
+
+class struct_tm(ctypes.Structure):
+    _fields_ = [
+        ('tm_sec', ctypes.c_int),
+        ('tm_min', ctypes.c_int),
+        ('tm_hour', ctypes.c_int),
+        ('tm_mday', ctypes.c_int),
+        ('tm_mon', ctypes.c_int),
+        ('tm_year', ctypes.c_int),
+        ('tm_wday', ctypes.c_int),
+        ('tm_yday', ctypes.c_int),
+        ('tm_isdst', ctypes.c_int),
+        ('tm_gmtoff', ctypes.c_long),
+        ('tm_zone', ctypes.c_int)]
+
+
+strptime_proto = ctypes.CFUNCTYPE(ctypes.c_char_p, ctypes.c_char_p,
+                                                   ctypes.c_char_p,
+                                                   ctypes.POINTER(struct_tm))
+
+strptime = strptime_proto(('strptime', libc), ((1, 's'),
+                                              (1, 'format'),
+                                              (2, 'tm')))
+
+
+def strptime_errcheck(result, func, args):
+    if result is None:
+        raise ValueError("strptime error.")
+
+    return datetime(
+        args[2].tm_year + 1900,
+        args[2].tm_mon,
+        args[2].tm_mday,
+        args[2].tm_hour,
+        args[2].tm_min,
+        args[2].tm_sec,
+    )
+
+strptime.errcheck = strptime_errcheck
 
 
 def date_range(str_d1, str_d2):
@@ -31,9 +76,9 @@ def get_datetime(row, index_name):
 
 
 def get_datetime_ex(row, regex, fmt):
-    m = re.search(regex.encode(), row)
+    m = re.search(regex, row)
     if not m:
         raise ValueError
-
-    dt = datetime.strptime(m.group(1).decode(), fmt)
+    dt = strptime(m.group(1), fmt)
+    return dt
     return str(dt.astimezone(timezone.utc).date())
