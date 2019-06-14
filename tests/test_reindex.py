ecb7a7088cdbd52945865fd62f083743a4e2f394
Dima Doroshev <dima@doroshev.com>
2019-06-14 16:06:44 +0300

    Add index

--- tests/test_reindex.py
+++ tests/test_reindex.py
@@ -1,5 +1,6 @@
 import json
 
+from tough.commands import reindex
 from tough.commands.reindex import run_reindex
 from tough.config import DATE_INDEX_NAME, INDEX_DIR
 
@@ -33,3 +34,31 @@ def test_reindex_twice(provide_data, index_name):
     run_reindex("")
     actual_index = json.load(open(INDEX_DIR / index_name / DATE_INDEX_NAME))
     assert actual_index == expected_index
+
+
+def test_reindex_only_changed_file(provide_data, index_name, data_dir, monkeypatch):
+    run_reindex(index_name)
+
+    fmt = (
+        '127.0.0.1 - - [{date}:12:34:56 +0000] "GET /{url} HTTP/1.1" 404 233 "-" "-"\n'
+    )
+
+    files_dir = data_dir / index_name
+    with open(files_dir / index_name, "a") as f:
+        f.write(fmt.format(date="25/Feb/2019", url="foo"))
+
+    real_add_to_index = reindex.add_to_index
+    add_to_index_paths = []
+
+    def patched_add_to_index(path, index_name, *, pool):
+        add_to_index_paths.append(path)
+        return real_add_to_index(path, index_name, pool=pool)
+
+    monkeypatch.setattr(reindex, "add_to_index", patched_add_to_index)
+
+    run_reindex(index_name)
+    assert len(add_to_index_paths) == 1
+    assert add_to_index_paths[0] == "tests/data/access_log/access_log"
+
+    actual_index = json.load(open(INDEX_DIR / index_name / DATE_INDEX_NAME))
+    assert actual_index == {**expected_index, "2019-02-25": {"access_log": [110]}}
