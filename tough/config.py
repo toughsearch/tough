02e69570b320d6269e2d22e9b25999692ff180f9
Dima Doroshev <dima@doroshev.com>
2019-12-25 20:24:44 +0300

    refac1

--- tough/config.py
+++ tough/config.py
@@ -4,5 +4,5 @@ from pathlib import Path
 CONF_NAME = os.getenv("CONF_NAME", "conf.yaml")
 INDEX_DIR = Path(os.getenv("INDEX_DIR", ".index"))
 DATE_INDEX_NAME = "date_index"
-NUM_WORKERS = int(os.getenv("NUM_WORKERS", os.cpu_count()))
+NUM_WORKERS = int(os.getenv("NUM_WORKERS", str(os.cpu_count())))
 MIN_CHUNK_LENGTH = int(os.getenv("MIN_CHUNK_LENGTH", 300_000))
